ccbba420e9cb7b568f0b0a41f89a628f194b0567
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2024-02-13 16:23:25 +0100

    Datasets corrected

--- experiments/run_pca.sh
+++ experiments/run_pca.sh
@@ -3,8 +3,6 @@
 chmod +x pca.sh
 
 INCLUSION=("all" "dropped" "combined")
-DATASETS_NUMBERS=(1 2 4 5 6 7 8 10)
-DROPPED_DATASETS_NUMBERS=(2 4 5 7 8 10)
 DATASETS=("HumanDendriticCells.h5ad" "MouseCellAtlas.h5ad" "HumanPancreas.h5ad" "PBMC.h5ad" "CellLine.h5ad" "MouseRetina.h5ad" "MouseBrain.h5ad" "MouseHematopoieticStemProgenitorCells.h5ad")
 DROPPED_DATASETS=("MouseCellAtlas.h5ad" "HumanPancreas.h5ad" "PBMC.h5ad" "MouseRetina.h5ad" "MouseBrain.h5ad" "MouseHematopoieticStemProgenitorCells.h5ad")
 BATCH_OUT=("BO0-C2" "BO0-C2" "BO0-C5" "BO0-C2" "BO0-C3" "BO0-C2" "BO0-C2" "BO0-C2")
@@ -14,13 +12,12 @@ COMMON_SPACE=false
 for T in "${INCLUSION[@]}"; do
   if [[ "$T" != "all" ]]; then
     DATASETS=("${DROPPED_DATASETS[@]}")
-    DATASETS_NUMBERS=("${DROPPED_DATASETS_NUMBERS[@]}")
     BATCH_OUT=("${DROPPED_BATCH_OUT[@]}")
   fi
   echo "${DATASETS[@]}"
   # Loop through each dataset
-  for index in "${!DATASETS_NUMBERS[@]}"; do
-    echo "PCA for $T dataset${DATASETS_NUMBERS[$index]} ${DATASETS[$index]} ${BATCH_OUT[$index]}"
+  for index in "${!DATASETS[@]}"; do
+    echo "PCA for $T ${DATASETS[$index]} ${BATCH_OUT[$index]}"
     ./pca.sh "$T" \
      "${DATASETS[$index]}" \
      "${BATCH_OUT[$index]}" \
