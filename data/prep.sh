7f5ff05773855b4188d57edb14ac986be651405b
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2024-02-22 17:58:00 +0100

    move ds

--- data/prep.sh
+++ data/prep.sh
@@ -6,3 +6,9 @@ datasets=(dataset1 dataset2 dataset4 dataset5 dataset6 dataset7 dataset8 dataset
 for ds in "${datasets[@]}";do
   python prep_benchmark_ds.py --root_path "/home/bba1658/FedScGen/data/${ds}"  --dataset "$ds"
 done
+
+# move datasets to the datasets folder
+for ds in "${datasets[@]}";do
+  # find h5ad files and move them to the datasets folder
+  find "/home/bba1658/FedScGen/data/${ds}" -name "*.h5ad" -exec mv {} /home/bba1658/FedScGen/data/datasets/ \;
+done
