91e0908681c4cf06a41a76dd3e0a85bc5f3caa0e
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2024-03-11 16:36:18 +0100

    mv prep data

--- data/prep.sh
+++ data/prep.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 datasets=(dataset1 dataset2 dataset4 dataset5 dataset6 dataset7 dataset8 dataset10)
+root_dir="$(dirname "$PWD")"
 
 # loop through datasets
 for ds in "${datasets[@]}";do
-  python prep_benchmark_ds.py --root_path "/home/bba1658/FedScGen/data/${ds}"  --dataset "$ds"
-done
-
-# move datasets to the datasets folder
-for ds in "${datasets[@]}";do
-  # find h5ad files and move them to the datasets folder
-  find "/home/bba1658/FedScGen/data/${ds}" -name "*.h5ad" -exec mv {} /home/bba1658/FedScGen/data/datasets/ \;
+  python prep_benchmark_ds.py --root_path "${root_dir}/data/${ds}"  --dataset "$ds"
+  h5ad_file=$(find "${root_dir}/data/${ds}" -name "*.h5ad")
+  python "${root_dir}/scripts/pca_reduction_simple.py" --path "$h5ad_file" \
+    --n_components 20 \
+    --output_dir "$h5ad_file"
+  mv "$h5ad_file" "datasets/"
 done
