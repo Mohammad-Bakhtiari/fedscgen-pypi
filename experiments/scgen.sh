422dc7e5b7c56b0204f312881d159759984e088c
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2024-03-11 16:31:14 +0100

    PCA on output

--- experiments/scgen.sh
+++ experiments/scgen.sh
@@ -5,7 +5,7 @@ H5AD_FILE="$1"
 REMOVE_CELL_TYPES="$2"
 COMBINE="$3"
 DROP="$4"
-GPU="${5:-1}"
+GPU="${5:-0}"
 BATCH_SIZE="${6:-50}"
 Z_DIM="${7:-10}"
 
@@ -14,16 +14,16 @@ DATASET=$(echo "$H5AD_FILE" | cut -f 1 -d '.')
 
 # Setting up other variables based on the flags
 if [ "$COMBINE" = "true" ]; then
-  TARGET_FOLDER="combined"
+  INCLUSION="combined"
 elif [ "$DROP" = "true" ]; then
-  TARGET_FOLDER="dropped"
+  INCLUSION="dropped"
 else
-  TARGET_FOLDER="all"
+  INCLUSION="all"
 fi
 
 root_dir="$(dirname "$PWD")"
 raw="${root_dir}/data/datasets/${H5AD_FILE}"
-output_path="${root_dir}/results/scgen/centralized/${DATASET}/${TARGET_FOLDER}"
+output_path="${root_dir}/results/scgen/centralized/${DATASET}/${INCLUSION}"
 
 combine_flag=""
 if [ "$COMBINE" = "true" ]; then
@@ -45,3 +45,9 @@ python "${root_dir}/scripts/scgen.py" \
 --early_stopping_kwargs "{'early_stopping_metric': 'val_loss', 'patience': 20, 'threshold': 0, 'reduce_lr': True, 'lr_patience': 13, 'lr_factor': 0.1}" \
 --gpu "$GPU" \
 $combine_flag
+
+corrected_file="${output_path}/corrected.h5ad"
+# Running PCA reduction on results
+python "${root_dir}/scripts/pca_reduction_simple.py" --path "$corrected_file" \
+  --n_components 20 \
+  --output_dir "$corrected_file"
