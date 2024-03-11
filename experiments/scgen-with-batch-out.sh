4c834d0e5c4767a6cc366d0f1d24855b886ffe59
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2024-03-11 16:31:49 +0100

    scgen.py & PCA

--- experiments/scgen-with-batch-out.sh
+++ experiments/scgen-with-batch-out.sh
@@ -17,29 +17,32 @@ DATASET=$(echo "$H5AD_FILE" | cut -f 1 -d '.')
 
 # Setting up other variables based on the flags
 if [ "$COMBINE" = "true" ]; then
-  TARGET_FOLDER="combined"
+  INCLUSION="combined"
 elif [ "$DROP" = "true" ]; then
-  TARGET_FOLDER="dropped"
+  INCLUSION="dropped"
 else
-  TARGET_FOLDER="all"
+  INCLUSION="all"
 fi
+
+
+OLD_IFS="$IFS"
+IFS=',' read -r -a BATCH_OUT_VALUES <<< "$5"
+IFS="$OLD_IFS"
 
 root_dir="$(dirname "$PWD")"
 raw="${root_dir}/data/datasets/${H5AD_FILE}"
-output_path="${root_dir}/results/scgen/centralized/${DATASET}/${TARGET_FOLDER}"
+output_path="${root_dir}/results/scgen/centralized/${DATASET}/${INCLUSION}"
 
 combine_flag=""
 if [ "$COMBINE" = "true" ]; then
     combine_flag="--combine"
 fi
-echo $BATCH_OUT_VALUES
-for i in "${!BATCH_OUT_VALUES[@]}"; do
-  batch_out=${BATCH_OUT_VALUES[$i]}
-  output_path="${output_path}/BO${i}"
+echo "Batch out values: ${BATCH_OUT_VALUES[@]}"
+for batch_out in "${!BATCH_OUT_VALUES[@]}"; do
+  output_path="${output_path}/BO${batch_out}"
   mkdir -p "${output_path}"
-  echo "$batch_out"
-  # Running python scripts
-  python "${root_dir}/scripts/scgen_with_batch_out.py" \
+  echo "Running scgen with batch out: $batch_out"
+  python "${root_dir}/scripts/scgen.py" \
   --model_path "${root_dir}/models/centralized/${DATASET}" \
   --data_path "$raw" \
   --output_path "$output_path" \
@@ -49,9 +52,15 @@ for i in "${!BATCH_OUT_VALUES[@]}"; do
   --z_dim "$Z_DIM" \
   --hidden_layers_sizes "800,800" \
   --batch_size "$BATCH_SIZE" \
-  --batch_out "$batch_out" \
+  --target_batch "$batch_out" \
   --remove_cell_types "$REMOVE_CELL_TYPES" \
   --early_stopping_kwargs "{'early_stopping_metric': 'val_loss', 'patience': 20, 'threshold': 0, 'reduce_lr': True, 'lr_patience': 13, 'lr_factor': 0.1}" \
   --gpu "$GPU" \
   $combine_flag
+
+  # Running PCA reduction on results# Running PCA reduction on results
+  corrected_file="${output_path}/corrected.h5ad"
+  python "${root_dir}/scripts/pca_reduction_simple.py" --path "$corrected_file" \
+  --n_components 20 \
+  --output_dir "$corrected_file"
 done
