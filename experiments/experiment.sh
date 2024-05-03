d46e2b024f3584799d376e89d5aabc89fc248faa
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2024-05-03 17:05:40 +0200

    executable

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -16,6 +16,6 @@ chmod +x scgen-with-batch-out.sh
 echo "Running scgen with batch out"
 ./scgen-with-batch-out.sh 'HumanPancreas.h5ad' '' false false "0,1"
 
-
+chmod +x run-classification.sh
 echo "Running centralized classification using corrected data by scGen and FedscGen"
 ./run-classification.sh
