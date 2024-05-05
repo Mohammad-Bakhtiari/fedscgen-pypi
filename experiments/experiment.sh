3f8f0444a80a2591c5625fe106a8b502ea963aa0
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2024-05-05 20:34:15 +0200

    revert 0&1 BO scGen

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -14,7 +14,7 @@ echo "Running fedscgen"
 
 chmod +x scgen-with-batch-out.sh
 echo "Running scgen with batch out"
-./scgen-with-batch-out.sh 'HumanPancreas.h5ad' '' false false "0,1"
+./scgen-with-batch-out.sh 'HumanPancreas.h5ad' '' false false "0,1,2,3,4"
 
 chmod +x run-classification.sh
 echo "Running centralized classification using corrected data by scGen and FedscGen"
