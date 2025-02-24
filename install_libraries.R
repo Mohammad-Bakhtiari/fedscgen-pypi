8fa4b655f2d2235afc21e390cf4e67077163c68c
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-02-24 08:55:31 +0100

    Set mirror & devtools

--- install_libraries.R
+++ install_libraries.R
@@ -1,9 +1,11 @@
+# Set CRAN mirror explicitly
+options(repos = c(CRAN = "https://cloud.r-project.org/"))
 
 # Check for BiocManager and install if it's not already installed
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-# Install zellkonverter using BiocManager
+# Install anndata from CRAN
 install.packages("anndata")
 
 # Install Matrix from CRAN
@@ -12,8 +14,11 @@ install.packages("Matrix")
 # Install devtools from CRAN
 install.packages("devtools")
 
-# Use devtools to install lisi from GitHub
-devtools::install_github("immunogenomics/lisi")
-
+# Load devtools
 library(devtools)
-install_github('theislab/kBET')
+
+# Install lisi from GitHub using devtools
+install_github("immunogenomics/lisi")
+
+# Install kBET from GitHub using devtools
+install_github("theislab/kBET")
