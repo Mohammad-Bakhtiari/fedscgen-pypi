39c0314ca07dc63360264fa6318ea43f69dbe203
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-03-10 14:40:05 +0100

    fix approach

--- metrics/benchmark.py
+++ metrics/benchmark.py
@@ -130,11 +130,11 @@ def plot_and_save(all_metrics, plot_name):
     plot_metrics_with_circles(df, plot_name.replace(".", "-circle."))
 
 
-def benchmark_dataset(file_path, seed, n_components, batch_key, cell_key, ds_name):
+def benchmark_dataset(approach, file_path, seed, n_components, batch_key, cell_key, ds_name):
     results = anndata.read_h5ad(file_path)
     latent_adata = anndata.AnnData(results.obsm[f'pca_{n_components}'])
     latent_adata.obs = results.obs
-    metrics = benchmark(results, latent_adata, batch_key, cell_key, "FedscGen")
+    metrics = benchmark(results, latent_adata, batch_key, cell_key, approach)
     metrics["Dataset"] = ds_name
     metrics["Seed"] = seed
     return metrics
@@ -155,7 +155,7 @@ def find_all_corrected_files_path(scgen_res_dir, fedscgen_res_dir):
                 file_path = os.path.join(root, file)
                 seed = file_path.split('seed_')[1].split('/')[0] if 'seed_' in file_path else -1
                 fedscgen_corrected_files.append((file_path, seed))
-    return scgen_corrected_files, fedscgen_corrected_files
+    return ('scGen', scgen_corrected_files), ('FedscGen', fedscgen_corrected_files)
 
 def benchmark_all_datasets(fed_data_dir: str, cent_data_dir: str, inclusion: str, n_components: int, batch_key: str,
                            cell_key: str):
@@ -165,10 +165,10 @@ def benchmark_all_datasets(fed_data_dir: str, cent_data_dir: str, inclusion: str
             continue
         print(f"Processing {ds_name} {inclusion}...")
         corrected_files = find_all_corrected_files_path(os.path.join(cent_data_dir, ds_name, inclusion), os.path.join(fed_data_dir, ds_name, inclusion))
-        for files in corrected_files:
+        for approach, files in corrected_files:
             for file_path, seed in files:
                 try:
-                    metric = benchmark_dataset(file_path, seed, n_components, batch_key, cell_key, ds_name)
+                    metric = benchmark_dataset(approach, file_path, seed, n_components, batch_key, cell_key, ds_name)
                     df = pd.DataFrame([metric])
                     df.to_csv(output_file, mode='a', sep=",", index=False, header=not os.path.exists(output_file))
                 except Exception as e:
