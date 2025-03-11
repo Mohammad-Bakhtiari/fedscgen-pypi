b38f741e3cf027851087ff4bc1c1fae767d2270b
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-03-11 15:41:41 +0100

    simple

--- experiments/run-fedscgen.sh
+++ experiments/run-fedscgen.sh
@@ -20,20 +20,14 @@ for index in "${!DATASETS[@]}"
 do
   for inclusion in dropped combined
   do
-    dataset=${DATASETS[$index]}
-    dropped_celltypes=${DROPPED_CELLTYPES[$index]}
     combined=$([ "$inclusion" == "combined" ] && echo true || echo false)
     dropped=$([ "$inclusion" == "dropped" ] && echo true || echo false)
-    n_clients="2"
-    batches="0,1"
-    batch_out="0"
-    if [ "$dataset" == "HumanPancreas" ]; then
-      n_clients="5 4"
-      batches="0,1,2,3,4"
-      batch_out="0 1"
-    fi
-    echo -e "\e[31mRunning fedscgen for $dataset with $inclusion with combined=$combined and dropped=$dropped and dropped_celltypes=$dropped_celltypes and n_clients=$n_clients and batches=$batches on GPU $GPU\e[0m"
-    ./fedscgen.sh "$dataset.h5ad" "${dropped_celltypes}" $combined $dropped "$batch_out" "$n_clients" "$batches" "$GPU"&
+    n_clients=$([ "${DATASETS[$index]}" == "HumanPancreas" ] && echo "5 4" || echo "2")
+    batches=$([ "${DATASETS[$index]}" == "HumanPancreas" ] && echo "0,1,2,3,4" || echo "0,1")
+    batch_out=$([ "${DATASETS[$index]}" == "HumanPancreas" ] && echo "0 1" || echo "0")
+
+    echo -e "\e[31mRunning fedscgen for ${DATASETS[$index]} with $inclusion with combined=$combined and dropped=$dropped and dropped_celltypes=${DROPPED_CELLTYPES[$index]} and n_clients=$n_clients and batches=$batches on GPU $GPU\e[0m"
+    ./fedscgen.sh "${DATASETS[$index]}.h5ad" "${DROPPED_CELLTYPES[$index]}" $combined $dropped "$batch_out" "$n_clients" "$batches" "$GPU"&
     GPU=$((GPU+1))
     if [ $GPU -eq $NUM_GPUS ]; then
       wait
