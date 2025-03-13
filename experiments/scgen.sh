6112ab34297965aae09adab226d22a0bc12fb056
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-03-13 14:19:44 +0100

    debug

--- experiments/scgen.sh
+++ experiments/scgen.sh
@@ -6,8 +6,8 @@ REMOVE_CELL_TYPES="$2"
 COMBINE="$3"
 DROP="$4"
 GPU="${5:-0}"
-BATCH_SIZE="${6:-50}"
-Z_DIM="${7:-10}"
+
+echo "Args: h5ad file: $H5AD_FILE, remove cell types: $REMOVE_CELL_TYPES, combine: $COMBINE, drop: $DROP, gpu: $GPU"
 
 # DATASET is H5AD_FILE without the extension
 DATASET=$(echo "$H5AD_FILE" | cut -f 1 -d '.')
@@ -30,25 +30,25 @@ if [ "$COMBINE" = "true" ]; then
     combine_flag="--combine"
 fi
 
-mkdir -p "${output_path}"
-export CUBLAS_WORKSPACE_CONFIG=:4096:8
-python "${root_dir}/scripts/scgen.py" \
---model_path "${root_dir}/models/${DATASET}" \
---data_path "$raw" \
---output_path "$output_path" \
---epoch 100 \
---batch_key "batch" \
---cell_key "cell_type" \
---z_dim "$Z_DIM" \
---hidden_layers_sizes "800,800" \
---batch_size "$BATCH_SIZE" \
---remove_cell_types "$REMOVE_CELL_TYPES" \
---early_stopping_kwargs "{'early_stopping_metric': 'val_loss', 'patience': 20, 'threshold': 0, 'reduce_lr': True, 'lr_patience': 13, 'lr_factor': 0.1}" \
---gpu "$GPU" \
-$combine_flag
-
-corrected_file="${output_path}/corrected.h5ad"
-# Running PCA reduction on results
-python "${root_dir}/scripts/pca_reduction_simple.py" --path "$corrected_file" \
-  --n_components 20 \
-  --output_dir "$corrected_file"
+#mkdir -p "${output_path}"
+#export CUBLAS_WORKSPACE_CONFIG=:4096:8
+#python "${root_dir}/scripts/scgen.py" \
+#--model_path "${root_dir}/models/${DATASET}" \
+#--data_path "$raw" \
+#--output_path "$output_path" \
+#--epoch 100 \
+#--batch_key "batch" \
+#--cell_key "cell_type" \
+#--z_dim 10 \
+#--hidden_layers_sizes "800,800" \
+#--batch_size 50 \
+#--remove_cell_types "$REMOVE_CELL_TYPES" \
+#--early_stopping_kwargs "{'early_stopping_metric': 'val_loss', 'patience': 20, 'threshold': 0, 'reduce_lr': True, 'lr_patience': 13, 'lr_factor': 0.1}" \
+#--gpu "$GPU" \
+#$combine_flag
+#
+#corrected_file="${output_path}/corrected.h5ad"
+## Running PCA reduction on results
+#python "${root_dir}/scripts/pca_reduction_simple.py" --path "$corrected_file" \
+#  --n_components 20 \
+#  --output_dir "$corrected_file"
