0f2f29e111727cd9e607444d28eafee77a4a9cbd
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-03-13 14:26:59 +0100

    correct DSs

--- experiments/run-scgen.sh
+++ experiments/run-scgen.sh
@@ -5,11 +5,9 @@ AVAILABLE_GPUS="${1:-0,1,2,3}"
 declare -a TASK_QUEUE
 
 DATASETS=(MouseCellAtlas HumanPancreas PBMC MouseRetina MouseBrain MouseHematopoieticStemProgenitorCells)
-DROPPED_CELLTYPES=( ""
- "Epithelial,Dendritic,Smooth-muscle,NK"
+DROPPED_CELLTYPES=("Epithelial,Dendritic,Smooth-muscle,NK"
   "stellate,endothelial,mesenchymal,macrophage,mast,epsilon,schwann,t_cell,MHC class II"
    "Plasmacytoid dendritic cell,Megakaryocyte,Hematopoietic stem cell"
-   ""
    "ganglion,vascular_endothelium,horizontal,fibroblasts,microglia,pericytes,astrocytes"
    "Olfactory ensheathing cells,Choroid_plexus,Mitotic"
    "MPP,LTHSC,LMPP,Unsorted")
