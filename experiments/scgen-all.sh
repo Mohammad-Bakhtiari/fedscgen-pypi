1e3caf6eb3d7b8b9826ec577c4dce74897722901
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-03-18 18:16:21 +0100

    task_name corrected

--- experiments/scgen-all.sh
+++ experiments/scgen-all.sh
@@ -9,7 +9,7 @@ for seed in "${SEEDS[@]}"
 do
   for dataset in "${DATASETS[@]}"
   do
-    task_name="${DATASETS[$index]}-${seed}"
+    task_name="${dataset}-${seed}"
     task="$task_name|${dataset}.h5ad|''|false|false|_GPU_|$seed"
     TASK_QUEUE+=("$task")
   done
