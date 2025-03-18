c547180ef5a40db38b6f74d2282e0fadd37b6444
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-03-18 19:07:03 +0100

    cover all batch-outs

--- experiments/run-fedscgen.sh
+++ experiments/run-fedscgen.sh
@@ -4,7 +4,7 @@ AVAILABLE_GPUS="${1:-0,1,2,3}"
 declare -a TASK_QUEUE
 
 
-DATASETS=(MouseCellAtlas HumanPancreas PBMC MouseRetina MouseBrain MouseHematopoieticStemProgenitorCells)
+DATASETS=(HumanDendriticCells MouseCellAtlas HumanPancreas PBMC CellLine MouseRetina MouseBrain MouseHematopoieticStemProgenitorCells)
 DROPPED_CELLTYPES=( ""
  "Epithelial,Dendritic,Smooth-muscle,NK"
   "stellate,endothelial,mesenchymal,macrophage,mast,epsilon,schwann,t_cell,MHC class II"
@@ -20,17 +20,31 @@ do
   for inclusion in all dropped combined
   do
     dataset="${DATASETS[$index]}"
-    combined=$([ "$inclusion" == "combined" ] && echo true || echo false)
-    dropped=$([ "$inclusion" == "dropped" ] && echo true || echo false)
-    n_clients=$([ "${dataset}" == "HumanPancreas" ] && echo "5 4" || echo "2")
-    batches=$([ "${dataset}" == "HumanPancreas" ] && echo "0,1,2,3,4" || echo "0,1")
-    batch_out=$([ "${dataset}" == "HumanPancreas" ] && echo "0 1" || echo "0")
-    if [ "${dataset}" == "CellLine" ]; then
+    if [ "$inclusion" != "all" ] && { [ "$dataset" == "HumanDendriticCells" ] || [ "$dataset" == "CellLine" ]; }; then
+        continue
+    fi
+    if [ "$dataset" == "HumanPancreas" ]; then
+      batches="0,1,2,3,4"
+      if [ "$inclusion" == "all" ]; then
+        n_clients="5 4"
+        batch_out="0 1"
+      else
+        n_clients="5"
+        batch_out="0"
+      fi
+    elif [ "$dataset" == "CellLine" ]; then
       n_clients="3"
       batches="0,1,2"
+      batch_out="0"
+    else
+      n_clients="2"
+      batches="0,1"
+      batch_out="0"
     fi
-    task_name="${DATASETS[$index]}-${inclusion}"
-    task="$task_name|${DATASETS[$index]}.h5ad|${DROPPED_CELLTYPES[$index]:-''}|$combined|$dropped|$batch_out|$n_clients|$batches|_GPU_"
+    combined=$([ "$inclusion" == "combined" ] && echo true || echo false)
+    dropped=$([ "$inclusion" == "dropped" ] && echo true || echo false)
+    task_name="${dataset}-${inclusion}"
+    task="$task_name|${dataset}.h5ad|${DROPPED_CELLTYPES[$index]:-''}|$combined|$dropped|$batch_out|$n_clients|$batches|_GPU_"
     TASK_QUEUE+=("$task")
   done
 done
