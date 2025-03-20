14fb16507aab45c25a337152dc3473a52af7cdb1
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-03-20 13:56:01 +0100

    lisi simp corrected

--- metrics/lisi/evaluate.sh
+++ metrics/lisi/evaluate.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
-scenario=$1
-
-scenarios=("datasets")
 parent_dir="$(dirname "$PWD")"
 root_dir="$(dirname "$parent_dir")"
 INCLUSION_SCENARIOS=("all" "dropped" "combined")
 DATASETS=("CellLine" "PBMC" "HumanPancreas" "MouseRetina" "MouseBrain" "MouseHematopoieticStemProgenitorCells" "HumanDendriticCells" "MouseCellAtlas")
-DROPPED_DATASETS=("PBMC" "HumanPancreas" "MouseRetina" "MouseBrain" "MouseHematopoieticStemProgenitorCells" "MouseCellAtlas")
 
-
-if [[ "$scenario" == "datasets" ]]; then
-  for T in "${INCLUSION_SCENARIOS[@]}"; do
-    if [[ "$T" != "all" ]]; then
-      DATASETS=("${DROPPED_DATASETS[@]}")
+for inclusion in "${INCLUSION_SCENARIOS[@]}"; do
+  for dataset in "${DATASETS[@]}"; do
+    if [[ ("$dataset" == "HumanDendriticCells" ||  "$dataset" == "CellLine") && "$inclusion" != "all" ]]; then
+      continue
     fi
-
-    # Loop through each dataset
-    for index in "${!DATASETS[@]}"; do
-      n_clients=2
-      if [[ "${DATASETS[$index]}" == "HumanPancreas" ]]; then
+    n_clients=2
+    case "$dataset" in
+      "HumanPancreas")
         n_clients=5
-      elif [[ "${DATASETS[$index]}" == "CellLine" ]]; then
+        ;;
+      "CellLine")
         n_clients=3
-      fi
-      output_dir="${root_dir}/results/scgen/federated/${DATASETS[$index]}/${T}/BO0-C${n_clients}"
-      raw="${root_dir}/data/datasets/${DATASETS[$index]}.h5ad"
-      scgen="${root_dir}/results/scgen/centralized/${DATASETS[$index]}/${T}/corrected.h5ad"
-      fedscgen="${root_dir}/results/scgen/federated/${DATASETS[$index]}/${T}/BO0-C${n_clients}/fed_corrected.h5ad"
-      echo "Running lisi.R for ${DATASETS[$index]} in ${T} scenario"
-      Rscript lisi.R "${raw}" "${scgen}" "${fedscgen}" "${output_dir}"
-    done
-  done
-fi
+        ;;
+    esac
 
+    output_dir="${root_dir}/results/fedscgen/${dataset}/${inclusion}/BO0-C${n_clients}"
+    raw="${root_dir}/data/datasets/${dataset}.h5ad"
+    scgen="${root_dir}/results/scgen/${dataset}/${inclusion}/corrected.h5ad"
+    fedscgen="${root_dir}/results/fedscgen/${dataset}/${inclusion}/BO0-C${n_clients}/fed_corrected.h5ad"
+
+    echo "Running lisi.R for ${dataset} in ${inclusion} scenario"
+    Rscript lisi.R "${raw}" "${scgen}" "${fedscgen}" "${output_dir}"
+  done
+done
