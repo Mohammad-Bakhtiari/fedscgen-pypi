2092b49dca9f31922e8aad2fbf08bce46b16506c
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-03-23 13:41:42 +0100

    benchmarks

--- metrics/benchmark.sh
+++ metrics/benchmark.sh
@@ -1,70 +1,30 @@
 #!/bin/bash
 
 scenario=$1
-plot_only=$2
+plot_only=${2:-false}
 
 plot_only_flag=""
 if [ "$plot_only" = "true" ]; then
   plot_only_flag="--plot_only"
 fi
 
-scenarios=("datasets" "snapshots" "batch-out" "tuning")
+# scenarios ==> "approach" "tuning"
+
 root_dir="$(dirname "$PWD")"
 
 DATASETS=("CellLine" "PBMC" "HumanPancreas" "MouseRetina" "MouseBrain" "MouseHematopoieticStemProgenitorCells" "HumanDendriticCells" "MouseCellAtlas")
-#DATASETS=("MouseBrain")
 
-# datasets
-if [ "$scenario" = "datasets" ]; then
-  for inclusion in "all" "dropped" "combined"; do
-    python benchmark.py --data_dir "${root_dir}/results/scgen/centralized" \
-    --fed_data_dir "${root_dir}/results/scgen/federated/smpc" \
-    --scenarios "datasets" \
-    --inclusion "${inclusion}" &
+# Approach
+if [ "$scenario" = "approach" ]; then
+  for approach in scgen fedscgen fedscgen-smppc; do
+    python benchmark.py --data_dir "${root_dir}/results" \
+    --approach "$approach" \
+    --scenarios "approach" &
   done
   wait
-fi
-
-# snapshot
-if [ "$scenario" = "snapshots" ]; then
-  python benchmark.py --data_dir "${root_dir}/results/scgen/batchout/snapshot" \
-  --scenarios "snapshots" \
-  --inclusion "all" \
-  --cell_key "cell_type" \
-  --batch_key "batch" \
-  --n_components 20 \
-  --n_rounds 10 \
-  $plot_only_flag
-fi
-
-# batch_out
-if [ "$scenario" = "batch-out" ]; then
-  python benchmark.py --data_dir "${root_dir}/results/scgen/centralized/HumanPancreas/all" \
-  --fed_data_dir "${root_dir}/results/scgen/federated/HumanPancreas/all/BO1-C4" \
-  --scenarios "batch-out" \
-  --inclusion all \
-  --cell_key "cell_type" \
-  --batch_key "batch" \
-  --n_batches 5 \
-  --n_components 20
 fi
 
 # Hyperparameter tuning rounds & epochs
 if [ "$scenario" = "tuning" ]; then
-  for dataset in "${!DATASETS[@]}"; do
-    if [ "${DATASETS[$dataset]}" = "MouseBrain" ]; then
-      continue
-    fi
-    target_dir="${root_dir}//results/scgen/federated/param-tuning/${DATASETS[$dataset]}"
-    echo "Running benchmark for param tuning of FedScGen for ${DATASETS[$dataset]}"
-    # Copy corrected files by ScGen
-    cp "${root_dir}/results/scgen/centralized/${DATASETS[$dataset]}/all/corrected.h5ad" "${target_dir}/scGen.h5ad"
-
-    python benchmark.py --data_dir "${target_dir}" \
-    --scenarios "${scenario}" \
-    --inclusion "all" \
-    --cell_key "cell_type" \
-    --n_components 20 &
-  done
-  wait
+  python benchmark.py --data_dir "/home/bba1658/FedscGen/results/fedscgen/param-tuning" --scenarios "tuning"
 fi
