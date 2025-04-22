fd5e15a3b70e67912bae8b7f67f0a4133b84e4cc
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-04-22 13:10:00 +0200

    finalized experiments

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-NUM_GPUS="${1:-3}"
+AVAILABLE_GPUS="${1:-0,1,2,3}"
 
 chmod +x run-scgen.sh
 echo "Running scgen for combined and dropped inclusion scenarios"
-./run-scgen.sh "${NUM_GPUS}"
+./run-scgen.sh "${AVAILABLE_GPUS}"
 
 chmod +x scgen-all.sh
 echo "Running scGen with different seeds for all inclusion scenario"
-./scgen-wilcoxon.sh "${NUM_GPUS}"
+./scgen-all.sh "${AVAILABLE_GPUS}"
 
 chmod +x scgen-with-batch-out.sh
 echo "Running scgen on HumanPancreas with batch out"
 ./scgen-with-batch-out.sh 'HumanPancreas.h5ad' '' false false "0,1,2,3,4"
 
 chmod +x run-tuning.sh
-echo "Running tuning"
-./run-tuning.sh "${NUM_GPUS}"
+echo "Running Hyper-parameters tuning for FedscGen without SMPC"
+./run-tuning.sh "${AVAILABLE_GPUS}"
 
 chmod +x run-fedscgen.sh
-echo "Running secure fedscgen for dropped and combined inclusion scenarios"
-./run-fedscgen.sh  "${NUM_GPUS}"
+echo "Running FedscGen for every inclusion scenarios"
+./run-fedscgen.sh  "${AVAILABLE_GPUS}"
 
-chmod +x fedscgen-all.sh
-echo "Running secure FedscGen with different seeds for all inclusion scenario"
-./fedscgen-all.sh "${NUM_GPUS}"
+chmod +x run-fedscgen-smpc.sh
+echo "Running FedscGen-SMPC with different seeds for the 'all' inclusion scenario"
+./run-fedscgen-smpc.sh "${AVAILABLE_GPUS}"
 
 chmod +x run-classification.sh
 echo "Running centralized classification using corrected data by scGen and FedscGen"
