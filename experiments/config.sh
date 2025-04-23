50068e99d9db13e72f723d2565010d53b418256e
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2025-04-23 15:18:36 +0200

    exec ready

--- experiments/config.sh
+++ experiments/config.sh
@@ -1,8 +1,8 @@
 # config.sh
 
-#SEEDS=(42 123 456 789 101112 2024 31415 2718 1618 9999)
-#
-#DATASETS=(HumanDendriticCells MouseCellAtlas HumanPancreas PBMC CellLine MouseRetina MouseBrain MouseHematopoieticStemProgenitorCells)
+SEEDS=(42 123 456 789 101112 2024 31415 2718 1618 9999)
+
+DATASETS=(HumanDendriticCells MouseCellAtlas HumanPancreas PBMC CellLine MouseRetina MouseBrain MouseHematopoieticStemProgenitorCells)
 CELLTYPES=( ""
  "Epithelial,Dendritic,Smooth-muscle,NK"
   "stellate,endothelial,mesenchymal,macrophage,mast,epsilon,schwann,t_cell,MHC class II"
@@ -11,10 +11,6 @@ CELLTYPES=( ""
    "ganglion,vascular_endothelium,horizontal,fibroblasts,microglia,pericytes,astrocytes"
    "Olfactory ensheathing cells,Choroid_plexus,Mitotic"
    "MPP,LTHSC,LMPP,Unsorted")
-
-SEEDS=(123)
-
-DATASETS=(MouseCellAtlas)
 
 DROPPED_DATASETS=(MouseCellAtlas HumanPancreas PBMC MouseRetina MouseBrain MouseHematopoieticStemProgenitorCells)
 
